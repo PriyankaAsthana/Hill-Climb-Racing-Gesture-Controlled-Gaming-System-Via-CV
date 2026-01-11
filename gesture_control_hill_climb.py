a2c8ab86b076a4c74fdb4a883829c125a9b02917
Priyanka Asthana <166942678+PriyankaAsthana@users.noreply.github.com>
2026-01-11 20:58:49 +0530

    Add files via upload

---
+++ gesture_control_hill_climb.py
@@ -0,0 +1,29 @@
+# Importing required libraries
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+from pynput.keyboard import Key, Controller
+
+# Initializing camera and hand detector
+cap = cv2.VideoCapture(0)
+cap.set(3, 720)
+cap.set(4, 420)
+detector = HandDetector(detectionCon=0.7, maxHands=1)
+keyboard = Controller()
+
+while True:
+    _, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        fingers = detector.fingersUp(hands[0])
+        if fingers == [0, 0, 0, 0, 0]:
+            keyboard.press(Key.left)
+            keyboard.release(Key.right)
+        elif fingers == [1, 1, 1, 1, 1]:
+            keyboard.press(Key.right)
+            keyboard.release(Key.left)
+    else:
+        keyboard.release(Key.left)
+        keyboard.release(Key.right)
+    cv2.imshow("Gesture-Controlled Hill Climb Racing", img)
+    if cv2.waitKey(1) == ord("q"):
+        break
